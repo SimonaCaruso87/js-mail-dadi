d22a203acc603787482584bce1a4647ef1cdf157
SimonaCaruso87 <caruso.simona87@gmail.com>
2023-06-16 00:28:07 +0200

    Fine Esercizio

--- gioco dei dadi/js/script.js
+++ gioco dei dadi/js/script.js
@@ -11,11 +11,37 @@
 
 //1)Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
 
-const nutUser = Math.floor((Math.random(1)*6)+1);
+const myButton = document.getElementById('click-button');
+myButton.addEventListener('click' , 
+      function (){
+        const nutUser = Math.floor((Math.random(1)*6)+1);
 
-const nutComputer = Math.floor((Math.random(1)*6)+1);
+        const nutComputer = Math.floor((Math.random(1)*6)+1);
 
-console.log('numero casuale', Math.floor((Math.random(1)*6)+1));
+        console.log('numero casuale', Math.floor((Math.random(1)*6)+1));
+
+        const valueNut = document.querySelector('.nut-user-html');
+        valueNut.innerHTML = nutUser ; 
+
+        const valueNutComputer = document.querySelector('.nut-computer-html');
+        valueNutComputer.innerHTML = nutComputer ; 
+
+
+        if(nutUser > nutComputer){
+            console.log('Hai Vinto' , nutUser , typeof nutUser);
+            const winner = document.querySelector('#winner');
+            winner.innerHTML = 'Hai Vinto';
+        }
+        else if(nutComputer > nutUser){
+            console.log('Hai Perso' , nutComputer , typeof nutComputer);
+            const winner = document.querySelector('#winner');
+            winner.innerHTML = 'Hai Perso';
+
+        }
+      }
+
+);
+
 
 
 
