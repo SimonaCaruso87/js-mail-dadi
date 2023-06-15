e3d76fe80fe57e67b25e77e757cee6408e7ad996
SimonaCaruso87 <caruso.simona87@gmail.com>
2023-06-15 18:26:05 +0200

    Esercizio mail completato

--- mail/js/script.js
+++ mail/js/script.js
@@ -2,42 +2,83 @@
    CONSEGNA
    1)Chiedi all'utente la sua e-mail
    2)Verifichiamo che sia nella lista di chi può accedere
+       -Se l'email è corretta andare avanti
+       -Se l'email non è corretta torna al punto 2
+
    3)Stampa un messaggio appropriato sull'esito del controllo
+        -Si L'email è registrata Accedi
+        -No l'email non è registrata riinserisci la tua email
 
 */   
 
-console.log('---------------------');
-console.log('LISTA DELLE EMAIL    ');
-console.log('---------------------');
+console.log('----------------------------');
+console.log('LISTA DELLE EMAIL REGISTRATE');
+console.log('----------------------------');
 
-const listaEmail = [
-    'Utente 1',
-    'Utente 2',
-    'Utente 3',
-    'Utente 4',
-    'Utente 5'
+const listaEmailRegistrate = [
+    'caruso.simona87@gmail.com',
+    'silvio@arcore.com',
+    'boolean.miverraunesauriemnto@speriamodino.com',
+    'mauro@aiutamitiprego.com',
+    'alessio@lightcoral.it'
 ]
 
-console.log('Lista Delle Email', listaEmail ,typeof listaEmail);
+console.log('Lista Delle Email', listaEmailRegistrate ,typeof listaEmailRegistrate);
 
+//Dichiariamo una variabile email list registrata che andrà nell'html
+
+//1)Chiedi all'utente la sua e-mail
 const emailList = document.getElementById('email-list');
 
-for (let i = 0; i < listaEmail.length; i++) {
-    console.log('-' , listaEmail[1]);
-
-    const newLi = document.createElement('li');
-    newLi.append(listaEmail[i]);    
-}
 
 const form = document.getElementById('login-email');
 
+
+//Quando l'utente sottomette il form deve accadere questa cosa
 form.addEventListener('submit' , function(event) {
     //preventDefault serve a non far sottomettere il form
     event.preventDefault();
+    //stampiamo in console
     console.log('Form Sottomesso');
 
-    const emailInput = document.getElementById('email');
+    const emailValue = document.getElementById('email').value;  
 
-    document.getElementById('email').value = '';   
+    //crea un ciclo definito di controllo sulle email
+
+    //d base l'utente non avrà l'accesso
+    let accessoUtenteAutorizzato = false ;
+
+    //inizializzazione contatore | condizione di permanenza | modalita di avanzamento ciclo
+    for (let i = 0; i < listaEmailRegistrate.length; i++) {
+        console.log('-' , listaEmailRegistrate[i]);
+    
+        if(listaEmailRegistrate[i] == emailValue ){
+             
+            console.log(listaEmailRegistrate , 'Email già registrata' , typeof listaEmailRegistrate);
+          
+            accessoUtenteAutorizzato = true ;
+
+        }
+        
+    }
+    
+
+    if(accessoUtenteAutorizzato == false){
+       
+        console.log(accessoUtenteAutorizzato , "Non sei benvenuto neppure all'inferno" , typeof accessoUtenteAutorizzato);
+
+        emailList.innerHTML = "Non sei benvenuto neppure all'Inferno" ; 
+
+    }
+    else {
+
+        console.log("Benvenuto all'Inferno Silvio");
+        emailList.innerHTML = "Benvenuto all'Inferno Silvio" ; 
+
+    }
+
+
+
+    document.getElementById('email').value = ''; 
 
 });
